e6c4616c376c86222474c7ea84fa60b29776975b
Tomas <tomaslonnblad@gmail.com>
2025-11-10 15:06:20 +0100

    Updated page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,6 @@
 import Image from "next/image";
-import bg from "../../public/background/home-background.png";
 import RenderModel from "@/components/RenderModel";
-// import Wizard from "@/components/models/Wizard";
 import Navigation from "@/components/navigation";
-
 import dynamic from "next/dynamic";
 const Wizard = dynamic(() => import("@/components/models/Wizard"), {
   ssr: false,
@@ -15,10 +12,11 @@ export default function Home() {
       <Image
         priority
         sizes="100vw"
-        src={bg}
+        src="/background/home-background.png"
         alt="background-image"
         fill
         className="-z-50 w-full h-full object-cover object-center opacity-50"
+        unoptimized
       />
 
       <div className="w-full h-screen">
